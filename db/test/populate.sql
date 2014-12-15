796b06f62e6f726f8fe4d3d5b77f6c906cf47c41
Liza Carter <lizahcarter@gmail.com>
2014-12-15 14:10:09 -0600

    changed populate file for travis fix

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,7 @@
+insert into users(username, password, avatar, token) values ('a1', 'b', 'c', 'd');
+insert into users(username, password, avatar, token) values ('a2', 'b', 'c', 'd');
+insert into users(username, password, avatar, token) values ('a3', 'b', 'c', 'd');
+
+delete from users;
+
 insert into users (id, username, password, avatar, token) values (1, 'bob','$2a$08$LQ0Zwfk5.r5OdYWeGkd/K.GX3rd.XUxy2lFpDeyZLiS2xSlDDgO2q', 'a.png', 'tok');
